9450927529f398ddc78328573fef64bbc873c388
Andre Souza Lemos (Admin) <ja32733.admin@malwee.com.br>
2023-06-07 15:38:01 -0300

    fazendo chamada de api

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:list_crud_firebase/services/api_service/api_response.dart';
 import 'package:list_crud_firebase/services/api_service/api_service.dart';
+import 'package:list_crud_firebase/services/api_service/http_api_service.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -16,8 +19,11 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            
+          onPressed: () async {
+            ApiService httpApiService = HttpApiService();
+            final ApiResponse response = await httpApiService.get("/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc'");
+            log(response.body);
+            log(response.statusCode.toString());
           }, 
           child: const Text("chamada de api")
         ),
--- lib/services/api_service/http_api_service.dart
+++ lib/services/api_service/http_api_service.dart
@@ -1,17 +1,31 @@
+import 'dart:convert';
+
 import 'package:list_crud_firebase/services/api_service/api_response.dart';
 import 'package:list_crud_firebase/services/api_service/api_service.dart';
+import 'package:http/http.dart' as http;
 
 class HttpApiService implements ApiService{
+
+  final _url = "https://api.themoviedb.org";
+
   @override
-  Future<ApiResponse> get(String path) {
-    // TODO: implement get
-    throw UnimplementedError();
+  Future<ApiResponse> get(String path) async {
+    
+    final apiResponse = await http.get(Uri.parse(_url + path), headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzhjYWUwOWFhY2VlNDA5NWRhYjNmODhlOWUyZWQ0OSIsInN1YiI6IjY0N2YzZWJkOTM4MjhlMDBiZmEwMTJhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R0wh_xeU0qHtIvlc4Yo-BtsMKw1h9l6QxV7t8o6k_Rc"});
+    
+    final jsonApiResponse = jsonDecode(apiResponse.body);
+
+    return ApiResponse(body: jsonApiResponse, statusCode: apiResponse.statusCode);
+    
   }
 
   @override
-  Future<ApiResponse> post(String path, body) {
-    // TODO: implement post
-    throw UnimplementedError();
+  Future<ApiResponse> post(String path, body) async {
+
+    final apiResponse = await http.post(Uri.parse(_url));
+    // final jsonApiResponse = jsonEncode(body);
+
+    return ApiResponse(body: body, statusCode: apiResponse.statusCode);
   }
 
 }
